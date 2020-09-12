71b947b7bb329f9c03e07d9be884b9236852485d
Allison Ricker <alliricker@gmail.com>
2020-09-12 17:10:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-if board(index) == board(index)
+if board[index] == board[index]
   true
-elsif board(index) == board(" ")
+elsif board[index] == board[" "]
   false
   end
 end
